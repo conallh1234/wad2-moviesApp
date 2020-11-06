c07511a4f26f8d7cf009eff1bc518deb5fbf9391
Conall Heffernan <conallh1234@hotmail.com>
2020-11-06 14:28:44 +0000

    useMovie custom hook

--- src/pages/movieDetailsPage.js
+++ src/pages/movieDetailsPage.js
@@ -4,15 +4,11 @@ import PageTemplate from "../components/templateMoviePage";
 import {getMovie} from '../api/tmdb-api'
 import { Link, Route, withRouter } from "react-router-dom"
 import MovieReviews from "../components/movieReviews"
+import useMovie from "../hooks/useMovie";
 
 const MoviePage = props => {
   const { id } = props.match.params;
-  const [movie, setMovie] = useState(null);
-  useEffect(() => {
-    getMovie(id).then(movie => {
-      setMovie(movie);
-    });
-  }, [id]);
+  const [movie] = useMovie(id)
   return (
     <>
     {movie ? (
