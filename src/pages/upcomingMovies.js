fc2dce987b9566bc5e23e139a317c703336ec983
Conall Heffernan <conallh1234@hotmail.com>
2020-11-29 19:20:55 +0000

    Change upcoming movies page to use context

--- src/pages/upcomingMovies.js
+++ src/pages/upcomingMovies.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import PageTemplate from '../components/templateMovieListPage'
-import {MoviesContext} from '../contexts/upcomingMoviesContext'
+import {MoviesContext} from '../contexts/moviesContext'
 import AddToWatchListButton from '../components/buttons/addToWatchList'
 
 const UpcomingMoviesPage = () => {
   const context = useContext(MoviesContext);
-  const movies = context.movies.filter((m) => {  
+  const movies = context.upcoming.filter((m) => {  
     return !("favorite" in m);
   });
 
